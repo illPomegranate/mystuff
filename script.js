d54baf65d63a144da866b21f75b9dd2e6a187571
illPomegranate <151964393+illPomegranate@users.noreply.github.com>
2024-04-02 04:59:41 +0800

    replacements

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 const x = document.getElementById("tts");
 const textStory = document.getElementById('story');
-
-
-    textStory = textStory.innerHTML.replace(/?/g, "'");
+   
 x.addEventListener('timeupdate', function(){
-    
+    this is to disable the javascript
     document.getElementById('timeneto').innerHTML = x.currentTime;
     const currentTime = x.currentTime;
 
-    // Loop through your text elements and highlight the current word being spoken
+    
     textStory.querySelectorAll('span[data-start]').forEach(word => {
         const startTime = parseFloat(word.getAttribute('data-start'));
         const endTime = parseFloat(word.getAttribute('data-end'));
 
         if (currentTime >= startTime && currentTime <= endTime) {
-            word.classList.add('highlight'); // Apply your CSS class for highlighting
+            word.classList.add('highlight'); 
         } else {
             word.classList.remove('highlight');
         }
